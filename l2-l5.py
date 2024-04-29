34a87412c32e2c4fe9845e86cf379b7273ebce66
arjun-selvakumar <154307342+arjun-selvakumar@users.noreply.github.com>
2024-04-29 21:52:42 +0530

    Create l2-l5.py

---
+++ l2-l5.py
@@ -0,0 +1,8 @@
+# Tuple containing the names of months in a year
+months = ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+
+# Slice the tuple to get the first half of the months
+first_half_months = months[:len(months)//2]
+
+# Print the new tuple containing the names of the first half of the months
+print("Names of the first half of the months:", first_half_months)
