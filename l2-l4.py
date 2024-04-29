95284c1d24da7292872686db15cada5384fc648d
arjun-selvakumar <154307342+arjun-selvakumar@users.noreply.github.com>
2024-04-29 21:52:16 +0530

    Create l2-l4.py

---
+++ l2-l4.py
@@ -0,0 +1,9 @@
+# Sample tuples containing integers
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+
+# Concatenate the two tuples to create a new tuple
+new_tuple = tuple1 + tuple2
+
+# Print the new tuple
+print("Concatenated tuple:", new_tuple)
