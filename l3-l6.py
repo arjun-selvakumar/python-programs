9288509258e5af7cd3aa8ab5e20ae33f08be4f6e
arjun-selvakumar <154307342+arjun-selvakumar@users.noreply.github.com>
2024-04-29 22:00:32 +0530

    Create l3-l6.py

---
+++ l3-l6.py
@@ -0,0 +1,9 @@
+# Sample lists of keys and values
+keys = ['Name', 'Age', 'City']
+values = ['John', 25, 'New York']
+
+# Create dictionary from two lists
+result_dict = dict(zip(keys, values))
+
+# Print the resulting dictionary
+print("Dictionary from two lists:", result_dict)
