9ecee74c4314c8a5523c6797075c861b34e33ebf
arjun-selvakumar <154307342+arjun-selvakumar@users.noreply.github.com>
2024-04-29 21:39:58 +0530

    Create l1-q17.py

---
+++ l1-q17.py
@@ -0,0 +1,7 @@
+def string_length(s):
+    return len(s)
+
+# Example usage:
+input_string = input("Enter a string: ")
+length = string_length(input_string)
+print(f"Length of the string: {length}")
