7602acaba60b7709eb22bf92f634e75ef2a0e37d
arjun-selvakumar <154307342+arjun-selvakumar@users.noreply.github.com>
2024-04-29 22:00:56 +0530

    Create l3-l7.py

---
+++ l3-l7.py
@@ -0,0 +1,9 @@
+# Define inner dictionaries
+inner_dict1 = {'a': 1, 'b': 2, 'c': 3}
+inner_dict2 = {'x': 10, 'y': 20, 'z': 30}
+
+# Create dictionary of dictionaries
+outer_dict = {'dict1': inner_dict1, 'dict2': inner_dict2}
+
+# Print the resulting dictionary of dictionaries
+print("Dictionary of dictionaries:", outer_dict)
