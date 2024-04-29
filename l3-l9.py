73fd91ae067720353022cc8d4f34ad452184e841
arjun-selvakumar <154307342+arjun-selvakumar@users.noreply.github.com>
2024-04-29 22:01:41 +0530

    Create l3-l9.py

---
+++ l3-l9.py
@@ -0,0 +1,8 @@
+# Sample dictionary
+data = {'b': 2, 'a': 1, 'c': 3}
+
+# Sort the dictionary by keys
+sorted_data = dict(sorted(data.items()))
+
+# Print the sorted dictionary
+print("Sorted dictionary by keys:", sorted_data)
