5b7e8d14c7ee2e05280fcc0c8e1173bdedeb8d2e
arjun-selvakumar <154307342+arjun-selvakumar@users.noreply.github.com>
2024-04-29 21:51:46 +0530

    Create l2-l3.py

---
+++ l2-l3.py
@@ -0,0 +1,11 @@
+# Sample tuples representing dates (year, month, day)
+date1 = (2022, 10, 15)
+date2 = (2023, 5, 20)
+
+# Compare the two dates
+if date1 < date2:
+    print("The first date is earlier than the second date.")
+elif date1 > date2:
+    print("The first date is later than the second date.")
+else:
+    print("The first date is the same as the second date.")
