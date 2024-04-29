4a2ea4f338b079b898cede265e76df66ef5522ba
arjun-selvakumar <154307342+arjun-selvakumar@users.noreply.github.com>
2024-04-29 21:56:52 +0530

    Create l3-l1.py

---
+++ l3-l1.py
@@ -0,0 +1,10 @@
+# Create an empty dictionary
+square_dict = {}
+
+# Iterate over numbers from 1 to 5
+for num in range(1, 6):
+    # Assign the square of the number as the value to the dictionary with the number as the key
+    square_dict[num] = num ** 2
+
+# Print the resulting dictionary
+print(square_dict)
