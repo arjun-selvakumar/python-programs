4af7c4f8d927cfd76deba9a003ff1fe49549b476
arjun-selvakumar <154307342+arjun-selvakumar@users.noreply.github.com>
2024-04-29 21:39:42 +0530

    Create l1-q16.py

---
+++ l1-q16.py
@@ -0,0 +1,8 @@
+def concatenate_strings(str1, str2):
+    return str1 + str2
+
+# Example usage:
+s1 = input("Enter first string: ")
+s2 = input("Enter second string: ")
+concatenated_string = concatenate_strings(s1, s2)
+print("Concatenated string:", concatenated_string)
