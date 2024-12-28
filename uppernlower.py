6415d4101be556d6c1f501fbfa3ef355950808f8
juno <154307342+arjun-selvakumar@users.noreply.github.com>
2024-12-28 10:51:37 +0530

    Update uppernlower.py

--- uppernlower.py
+++ uppernlower.py
@@ -1,1 +1,8 @@
-
+sen = "Boku no nawa Monkey.D.Luffy"
+for i in range(0,len(sen)):
+    if i<=len(sen)//2:
+        let=sen[i]
+        print(let.lower(),end='')
+    else:
+        let=sen[i]
+        print(let.upper(),end='')
